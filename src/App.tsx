eb8d031471e3d216c01d6c7142d0abe45c008863
Markus <mgmarkusgoetz@web.de>
2019-11-10 10:52:24 +0100

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import {IonApp, IonRouterOutlet} from '@ionic/react';
 import {IonReactRouter} from '@ionic/react-router';
 import {Home} from './pages/Home';
 import {StorageProvider} from './provider/Storage';
+import {CreateEvent} from "./pages/CreateEvent";
+import {Create} from "./pages/Eventerstellen";
+import {CreateList} from "./pages/Mitbringliste";
 
 export const App: FC = () => (
   <StorageProvider>
     <IonApp>
       <IonReactRouter>
         <IonRouterOutlet>
-          <Route path="/home" component={Home} exact={true} />
-          <Route path="/event/add1" component={Create} exact={true} />
-          <Route path="/event/add2" component={CreateEvent} exact={true} />
-          <Route path="/event/add3" component={CreateList} exact={true} />
-          <Route exact path="/" render={() => <Redirect to="/home" />} />
+          <Route path="/home" component={Home} exact={true}/>
+          <Route path="/event/add1" component={Create} exact={true}/>
+          <Route path="/event/add2" component={CreateEvent} exact={true}/>
+          <Route path="/event/add3" component={CreateList} exact={true}/>
+          <Route exact path="/" render={() => <Redirect to="/home"/>}/>
         </IonRouterOutlet>
       </IonReactRouter>
     </IonApp>
